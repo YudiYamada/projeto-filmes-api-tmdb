6bd2aa17355d90518420949bf4525d407d4538e2
Yudi Yamada <yudiyamada10@gmail.com>
2025-05-06 20:02:00 -0300

    feat: update Footer and Header components, add MovieCard component, and modify global styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,33 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import "./styles.css";
+
 function Footer() {
-    return ( 
-        <>
-            <h2>Footer</h2>
-        </>
-     );
+  return (
+    <footer className="footer-container">
+      <ul className="social-list-ul">
+        <li>
+          <a
+            className="link-a"
+            href="https://www.linkedin.com/in/yudi-yamada-0a10181b9/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            className="link-a"
+            href="https://github.com/YudiYamada"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/logo.png";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import "./styles.css";
+
 function Header() {
-    return ( 
-        <>
-            <h2>Header</h2>
-        </>
-     );
+  return (
+    <div>
+      <nav id="navbar">
+        <h2>
+          <Link to="/"><img src={logo} alt="logo" /> Movies Library</Link>
+        </h2>
+        <form>
+          <input type="text" placeholder="Busque um filme" />
+          <button type="submit"><BiSearchAlt2 /></button>
+        </form>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
